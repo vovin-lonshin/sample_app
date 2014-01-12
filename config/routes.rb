567c8d5d14f3ff4d4b3560335e0ff7af49b331f2
Vovin Lonshin <vovin.lonshin@gmail.com>
2014-01-12 12:22:59 -0600

    Finsh user controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/home', to: 'static_pages#home', via: 'get'
+  match '/home',    to: 'static_pages#home',    via: 'get'
 
 
   # Example of regular route:
