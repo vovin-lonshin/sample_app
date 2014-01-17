86c88985f8f4d7ddec1e34a8b9f5cdf5d69455ea
Vovin Lonshin <vovin.lonshin@gmail.com>
2014-01-16 21:52:03 -0600

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ SampleApp::Application.routes.draw do
 
   
   resources :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
+  resources :sessions, only: [:new, :create, :destroy]
   
   root  'static_pages#home'
+  
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
