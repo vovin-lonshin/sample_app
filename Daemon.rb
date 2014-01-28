4f687f40114f38076dc2ef548455d5968f6200d7
Vovin Lonshin <vovin.lonshin@gmail.com>
2014-01-28 11:19:46 -0600

    Finish user edit, update, index, and destroy actions

---
+++ Daemon.rb
@@ -0,0 +1,21 @@
+﻿class  Daemon
+   def name(string)
+      num=3+rand(4)
+     dd=string.split.join.upcase
+     dd=dd.split(//).in_groups(num,"G")
+
+    (0..dd.length-1).each do |col|
+    
+       dd[col].each do |i|
+           if dd[col].length >1
+              dd[col] = dd[col].shuffle[0..-2]
+           else
+             dd[col] = dd[col]
+          end
+       end
+    end
+     name=dd
+  end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ SampleApp::Application.routes.draw do
   
   root  'static_pages#home'
   
+  match '/users',  to: 'users#index',           via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
